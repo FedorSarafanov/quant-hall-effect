add51c1e22064f09076bbf9b75f4b2433d6de8f7
Fedor Sarafanov <sfg180@yandex.ru>
2019-04-14 11:42:58 +0300

    Вычищены остатки Кирилла

--- hall.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-pdflatex.exe -synctex=1 -interaction=nonstopmode %~n0.tex >nul
-@echo on
-pythontex %~n0.tex
-@echo off
-pdflatex.exe -synctex=1 -interaction=nonstopmode %~n0.tex >nul
-start "D:\SumatraPDF\SumatraPDF.exe" %~n0.pdf
